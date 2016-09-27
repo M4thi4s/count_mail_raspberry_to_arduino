bca798f4d876460588fd17a7597cf39c76597b4d
m4thi4s <3l3ctro.m4thi4s@gmail.com>
2016-09-27 20:31:45 +0200

    Create send_rpi.py

---
+++ send_rpi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial
+import time
+ser = serial.Serial('/dev/ttyACM0', 9600) #you need to plug the rpi for use this program, if a bug is signaled in this line you need to change the file path
+time.sleep(3);    #reboot
+mon_fichier = open("nb_mail.txt", "r")
+contenu = mon_fichier.read(1)
+print (contenu)
+ser.write(str(contenu)) #send the number of mail to the rpi
+print ("finish")
