6f5d732d022e2a239c36a9a741b3b5078dcefc36
m4thi4s <3l3ctro.m4thi4s@gmail.com>
2016-09-28 14:08:04 +0200

    Update send_rpi.py

--- send_rpi.py
+++ send_rpi.py
@@ -4,9 +4,8 @@
 import serial
 import time
 ser = serial.Serial('/dev/ttyACM0', 9600) #you need to plug the rpi for use this program, if a bug is signaled in this line you need to change the file path
-time.sleep(3);    #reboot
+time.sleep(1);    #reboot arduino
 mon_fichier = open("nb_mail.txt", "r")
 contenu = mon_fichier.read(1)
-print (contenu)
 ser.write(str(contenu)) #send the number of mail to the rpi
-print ("finish")
+print ("send to arduino")
